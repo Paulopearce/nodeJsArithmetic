a0555e66e3d054ab1b049122c3d4f2abf103e98d
Paul Pearce <pearce.o.paul2020@gmail.com>
2021-08-30 11:08:21 -0700

    First program in node

---
+++ app.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+  console.log(`${num1} + ${num2} = ${num1+num2}`)
+}
+
+function subtract(num1, num2) {
+  console.log(`${num1} - ${num2} = ${num1-num2}`)
+}
+
+function multiply(num1, num2) {
+  console.log(`${num1} * ${num2} = ${num1*num2}`)
+}
+
+function divide(num1, num2) {
+  console.log(`${num1} + ${num2} = ${num1/num2}`)
+}
+
+add(1,2)
+
+subtract(2,3)
+
+multiply(3,4)
+
+divide(4,5)
